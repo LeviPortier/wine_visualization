8049e832929dfa8a9775a23d494b43a4b32f729c
LeviPortier <54774749+LeviPortier@users.noreply.github.com>
2021-04-06 09:23:47 +0200

    Fixed colors

--- paracoords.py
+++ paracoords.py
@@ -1,13 +1,10 @@
 import plotly.graph_objects as go
 import pandas as pd
 
-
-
-
 def create_paracoords(data):
     fig = go.Figure(data=
                 go.Parcoords(
-                    line = dict(color = data['quality'], colorscale = [[3,'#0d0887'], [4,'#46039f'], [5,['#7201a8'], [6,'#9c179e'], [7,'#bd3786'], [8,'#d8576b'], [9,'#ed7953']]),dimensions = list([
+                    line = dict(color = data['quality']),dimensions = list([
                         dict(constraintrange = [7.5,9]
                              label = 'quality', values = data['quality']),
                         dict(label = 'alcohol', values = data['alcohol']),
